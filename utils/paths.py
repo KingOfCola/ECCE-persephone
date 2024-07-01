03f94297454266ee2f8329866f333ffb3d188315
Urvan <urvan.christen@gmail.com>
2024-07-01 13:55:51 +0200

    EP-7 Create a path management system

--- utils/paths.py
+++ utils/paths.py
@@ -10,8 +10,17 @@
 
 
 import os
+import json
+import warnings
 
 __ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+__PATHS_JSON = "paths.json"
+
+__PATHS = None
+__PATH_STRUCTURE = {
+    "data": "Data folder",
+    "output": "Output folder",
+}
 
 
 def asset(path: str) -> str:
@@ -29,3 +38,107 @@ def asset(path: str) -> str:
         The full path to the asset file.
     """
     return os.path.join(__ROOT_PATH, "assets", path)
+
+
+def load_path_json() -> dict:
+    """
+    Load the paths from the paths.json file.
+
+    Returns
+    -------
+    dict
+        The paths as a dictionary.
+    """
+    global __PATHS
+
+    if __PATHS is not None:
+        return __PATHS
+
+    # Load the paths from the JSON file
+    path_json = asset(__PATHS_JSON)
+    if not os.path.exists(path_json):
+        create_path_json(paths=__PATH_STRUCTURE)
+        raise ValueError(
+            f"The paths file '{path_json}' does not exist. It has been created with default paths. Please fill it with the correct paths."
+        )
+
+    with open(path_json, "r") as f:
+        __PATHS = json.load(f)
+
+    if not validate_paths(paths=__PATHS):
+        raise ValueError(
+            f"The paths stored in '{path_json}' are not valid. Please check them."
+        )
+
+    return __PATHS
+
+
+def create_path_json(paths):
+    """
+    Create the paths.json file with the given paths.
+
+    Parameters
+    ----------
+    paths : dict
+        The paths to write to the JSON file.
+    """
+    # Get the path to the JSON file
+    path_json = asset(__PATHS_JSON)
+
+    # Write the paths to the JSON file
+    with open(path_json, "w") as f:
+        json.dump(paths, f, indent=4)
+
+    if not validate_paths(paths=paths):
+        warnings.warn(
+            f"The paths stored in '{path_json}' are not valid. Please check them."
+        )
+
+
+def validate_paths(paths: dict) -> bool:
+    """
+    Validate the paths dictionary.
+
+    Parameters
+    ----------
+    paths : dict
+        The paths to validate.
+
+    Returns
+    -------
+    bool
+        True if the paths are valid, False otherwise.
+    """
+    for key, value in __PATH_STRUCTURE.items():
+        if key not in paths:
+            return False
+        if not os.path.exists(paths[key]):
+            return False
+    return True
+
+
+def get_path_factory(root: str):
+    paths = load_path_json()
+    root_path = paths[root]
+
+    def get_path(path: str) -> str:
+        """
+        Get an absolute path from a path relative to a global root.
+
+        Parameters
+        ----------
+        path : str
+            The local path of the file relative to the root.
+
+        Returns
+        -------
+        str
+            The absolute path.
+        """
+        return os.path.join(root_path, path)
+
+    return get_path
+
+
+data = get_path_factory("data")
+output = get_path_factory("output")
