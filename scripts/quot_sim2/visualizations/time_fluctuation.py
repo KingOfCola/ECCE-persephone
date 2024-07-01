e8b3a5b1a50d1052bc6c00b414af4aa8469c664e
Urvan <urvan.christen@gmail.com>
2024-07-01 18:51:50 +0200

    EP-10 Exploration of time dependence

---
+++ scripts/quot_sim2/visualizations/time_fluctuation.py
@@ -0,0 +1,120 @@
+# -*-coding:utf-8 -*-
+"""
+@File      :   time_fluctuation.py
+@Time      :   2024/07/01 17:33:24
+@Author    :   Urvan Christen
+@Version   :   1.0
+@Contact   :   urvan.christen@gmail.com
+@Desc      :   Scripts for visualizing the time fluctuation of the temperature profiles
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+
+from utils.paths import data, output
+
+
+def autocorr4(x):
+    """fft, don't pad 0s, non partial"""
+    mean = x.mean()
+    var = np.var(x)
+    xp = x - mean
+
+    cf = np.fft.fft(xp)
+    sf = cf.conjugate() * cf
+    corr = np.fft.ifft(sf).real / var / len(x)
+
+    return corr
+
+
+if __name__ == "__main__":
+    FULL_YEAR_MIN = 1959
+    FULL_YEAR_MAX = 2023
+    YEARS = FULL_YEAR_MAX - FULL_YEAR_MIN + 1
+
+    DAYS_IN_YEAR = 365
+    N = YEARS * DAYS_IN_YEAR
+
+    temperatures_stations = pd.read_parquet(
+        data(r"Quot_SIM2/Preprocessed/1958_2024-05_T_Q.parquet")
+    )
+
+    temperatures_stations.reset_index(inplace=True)
+    temperatures_stations = temperatures_stations.loc[
+        (temperatures_stations["year"].between(FULL_YEAR_MIN, FULL_YEAR_MAX))
+        & (temperatures_stations["day_of_year"] <= DAYS_IN_YEAR)
+    ]
+
+    STATION = "S2000"
+
+    temperatures = temperatures_stations[STATION].values
+
+    fft = np.fft.fft(temperatures)
+    afft = np.abs(fft)
+
+    n_harmonics = 3
+
+    harmonics = np.zeros(n_harmonics, dtype=complex)
+    harmonics = fft[np.arange(n_harmonics) * YEARS] / N * 2
+    harmonics[0] /= 2
+
+    standardized_temperatures = np.copy(temperatures)
+    for i, a in enumerate(harmonics):
+        standardized_temperatures -= np.abs(a) * np.cos(
+            2 * np.pi * i * np.arange(N) / DAYS_IN_YEAR + np.angle(a)
+        )
+
+    # Plot the temperature profile
+    fig, ax = plt.subplots(2, 1, figsize=(10, 6), sharex=True)
+    fig.suptitle(f"Temperature profile of station {STATION}")
+
+    ax[0].plot(temperatures)
+    ax[0].set_ylabel("Mean temperature (absolute)")
+
+    ax[1].plot(standardized_temperatures)
+    ax[1].set_ylabel("Mean temperature (residuals)")
+    plt.show()
+
+    # QQ-plot
+    fig, ax = plt.subplots(1, 1, figsize=(10, 6))
+    sm.qqplot(standardized_temperatures, line="s", ax=ax)
+    plt.show()
+
+    # Autocorrelation
+    fig, ax = plt.subplots(1, 1, figsize=(10, 6))
+    t = np.arange(500)
+    auto = autocorr4(standardized_temperatures)
+    tauto = np.exp(t * np.log(auto[1]))
+    ax.plot(t, auto[t], "o")
+    ax.plot(t, tauto, "r")
+    plt.show()
+
+    # standardized_temperatures = np.random.normal(0, 1, N)
+
+    mean = np.mean(standardized_temperatures)
+    q1 = np.percentile(standardized_temperatures, 25)
+    q3 = np.percentile(standardized_temperatures, 75)
+    iqr = q3 - q1
+    std = (q3 - q1) / 1.349
+    binwidth = 2 * iqr / N ** (1 / 3)
+    x = np.linspace(mean - 4 * std, mean + 4 * std, 1000)
+    normal_distribution = np.exp(-0.5 * ((x - mean) / std) ** 2) / (
+        std * np.sqrt(2 * np.pi)
+    )
+    ci_inf = normal_distribution - 1.96 * np.sqrt(
+        normal_distribution * (1 - normal_distribution) / (N * binwidth)
+    )
+    ci_sup = normal_distribution + 1.96 * np.sqrt(
+        normal_distribution * (1 - normal_distribution) / (N * binwidth)
+    )
+
+    fig, ax = plt.subplots(1, 1, figsize=(10, 6))
+    sns.histplot(
+        standardized_temperatures, binwidth=binwidth, kde=True, ax=ax, stat="density"
+    )
+    ax.plot(x, normal_distribution, label="Normal distribution", color="red")
+    ax.fill_between(x, ci_inf, ci_sup, color="red", alpha=0.3)
+    plt.show()
